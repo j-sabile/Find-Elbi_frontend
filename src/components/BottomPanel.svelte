af52738b30ad87c29bef2e8d9935ad961e7ee37b
jpsabile <jpsabile@up.edu.ph>
2023-10-20 09:36:11 +0800

    fix bug in bottomPanel

--- src/components/BottomPanel.svelte
+++ src/components/BottomPanel.svelte
@@ -6,16 +6,18 @@
   let pillExtended = false;
 </script>
 
-<div class="flex flex-col absolute bg-white w-full box-border rounded-t-2xl bottompane {closed ? '' : pillExtended ? 'extended' : 'semi-extended'} {classes}" transition:slide>
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div class="w-full bg-white rounded-t-2xl flex justify-center cursor-pointer" on:click={() => (pillExtended = !pillExtended)}>
-    <div class="pill bg-neutral-300 rounded-full shadow-sm m-2" />
+{#if !closed}
+  <div class="flex flex-col absolute bg-white w-full box-border rounded-t-2xl bottompane {closed ? '' : pillExtended ? 'extended' : 'semi-extended'} {classes}" transition:slide>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="w-full bg-white rounded-t-2xl flex justify-center cursor-pointer" on:click={() => (pillExtended = !pillExtended)}>
+      <div class="pill bg-neutral-300 rounded-full shadow-sm m-2" />
+    </div>
+    <div class="w-full overflow-auto">
+      <slot />
+    </div>
   </div>
-  <div class="w-full overflow-auto">
-    <slot />
-  </div>
-</div>
+{/if}
 
 <style>
   .bottompane {
