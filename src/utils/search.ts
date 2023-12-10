df938c42ff9041b938d088a4bb9872e683644003
j-sabile <jericosabile@gmail.com>
2023-12-10 19:10:13 +0800

    optimize serach function

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -2,6 +2,8 @@ import Fuse from "fuse.js";
 import buildings from "../data/buildings";
 import type { IBuilding } from "../interfaces/IBuilding";
 
+const THRESHOLD = 0.3;
+
 const fuse = new Fuse(buildings, {
   keys: [
     { name: "name", weight: 0.4 },
@@ -14,6 +16,13 @@ const fuse = new Fuse(buildings, {
 
 const search = (input: string) => {
   let temp = fuse.search(input);
+  if (temp.length > 0) {
+    let newTemp = [];
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i].score! > temp[0].score! + THRESHOLD) return newTemp;
+      newTemp.push(temp[i].item);
+    }
+  }
   return temp.map((i) => i.item) as IBuilding[];
 };
 
